6517f87ba6ed25dcd9c98bb306d5db7228a1c5b3
Your Name <you@example.com>
2019-03-11 14:36:50 +0800

    1-完成webpack_react项目环境搭建

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+//导入根组件
+import App from './App.jsx'
+
+//使用ReactDOM渲染根组件的内容到id=root的div
+ReactDOM.render(<App/>,document.getElementById("root"))
